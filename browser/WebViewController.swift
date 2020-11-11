1de3dc80823d5304cf4bc9a504c032567d0d8034
Marat <>
2020-11-11 20:45:06 +0200

    init

---
+++ browser/WebViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  browser
+//
+//  Created by Marat on 08/11/2020.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+
+    // MARK: UI
+    @IBOutlet weak var addressBarTF: UITextField!
+    @IBOutlet weak var webview: WKWebView!
+    // MARK: Data
+
+    // MARK: Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
